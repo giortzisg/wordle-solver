447c51f69c965d5219cb912b5b4cfd2176fdd46f
Giannis Giortzis <giortzis.giannis@gmail.com>
2023-07-06 13:41:09 +0300

    fix probability map path

--- script/calculate_probability.py
+++ script/calculate_probability.py
@@ -1,16 +1,22 @@
 import json
 import math
+import os 
+from pathlib import Path
 
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = Path(SCRIPT_DIR).parent.absolute()
 
 def sigmoid(pos):
     return 1 / (1 + math.exp(-pos))
 
-with open('internal/entities/frequency_map.json') as json_file:
+# create a probability map for all words depending on their frequency for faster runtime computation
+# the frequency_map.json file has the used frequency of the specified words in the english vocabulary
+with open(str(ROOT_DIR) + '/config/frequency_map.json') as json_file:
     data = json.load(json_file)
     sorted = list(sorted(data.items(), key=lambda item: item[1]))
     word_possibility = {}
     for x in range(len(sorted)):
         word_possibility[sorted[x][0]] = sigmoid((x*12-117800)/2203)
  
-    with open('internal/entities/probability_map.json', 'w', encoding='utf-8') as f:
+    with open(str(ROOT_DIR) + '/config/probability_map.json', 'w', encoding='utf-8') as f:
         json.dump(word_possibility, f, ensure_ascii=False, indent=4)
